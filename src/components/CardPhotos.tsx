3fd2f2dce7f13fcaec867ae3f7f2a9834bdc5986
mahdiharish <mahdiharish12@gmail.com>
2023-06-08 21:57:35 +0800

    fix like button

--- src/components/CardPhotos.tsx
+++ src/components/CardPhotos.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from 'react';
 
 type CardProps = {
-    id: number;
-    name: string;
-    imgUrl: string;
-    isLiked: boolean;
+  id: number;
+  name: string;
+  imgUrl: string;
+  isLiked: boolean;
 };
 
-const Card: React.FC<CardProps> = ({ name, imgUrl, isLiked }) => {
-    return (
-        <div>
-            <img src={imgUrl} alt={name} />
-            <p>{name}</p>
-            <p>{isLiked ? "Liked" : "Like"}</p>
-        </div>
-    );
+const Card: React.FC<CardProps> = ({ id, name, imgUrl, isLiked }) => {
+  const [isCardLiked, setIsCardLiked] = useState(isLiked);
+
+  const handleLike = () => {
+    setIsCardLiked(!isCardLiked);
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleLike}>
+        {isCardLiked ? 'Liked' : 'Like'}
+      </button>
+      <img src={imgUrl} alt={name} />
+      <p>{name}</p>
+    </div>
+  );
 };
 
-export default Card
+export default Card;
