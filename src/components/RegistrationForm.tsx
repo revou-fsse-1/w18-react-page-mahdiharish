97736f81364dcc8b79d59a43490834c1de56c1ec
mahdiharish <mahdiharish12@gmail.com>
2023-06-09 21:04:40 +0800

    registration form completed

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type RegistrationFormProps = {
   onClose: () => void;
@@ -22,9 +22,9 @@ const RegistrationForm: React.FC<RegistrationFormProps> = ({
     setFirstNameError("");
     setLastNameError("");
     const isValidEmail = (email: string) => {
-    const emailRegex = /\S+@\S+\.\S+/;
-    return emailRegex.test(email);
-  };
+      const emailRegex = /\S+@\S+\.\S+/;
+      return emailRegex.test(email);
+    };
 
     let isValid = true;
     if (email.trim() === "") {
@@ -44,43 +44,75 @@ const RegistrationForm: React.FC<RegistrationFormProps> = ({
     }
     if (isValid) {
       onSubmit();
+      onClose();
     }
   };
 
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      const form = document.getElementById("registration-form");
+      if (form && !form.contains(e.target as Node)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
-    <div>
-      <h2>Register to Photo Club Member</h2>
-      <p>Get countless benefits by joining us!</p>
+    <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center z-40">
+      <div className="bg-black bg-opacity-70 fixed top-0 left-0 w-full h-full" />
 
-      <form onSubmit={handleSubmit}>
-        <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        {emailError && <p>{emailError}</p>}
+      <div
+        id="registration-form"
+        className="relative z-50 bg-white p-8 rounded-lg text-center"
+      >
+        <h2 className="text-2xl font-bold mb-2">Register to Photo Club Member</h2>
+        <p className="mb-4">Get countless benefits by joining us!</p>
 
-        <label>First Name:</label>
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        {firstNameError && <p>{firstNameError}</p>}
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+          <label>Email:</label>
+          <input
+            type="email"
+            placeholder="Insert your email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full mb-2 border border-gray-300 rounded-lg px-4 py-2"
+          />
+          {emailError && <p>{emailError}</p>}
 
-        <label>Last Name:</label>
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        {lastNameError && <p>{lastNameError}</p>}
+          <label>First Name:</label>
+          <input
+            type="text"
+            placeholder="Insert your first name..."
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            className="w-full mb-2 border border-gray-300 rounded-lg px-4 py-2"
+          />
+          {firstNameError && <p>{firstNameError}</p>}
 
-        <button type="submit">Register Now</button>
-      </form>
+          <label>Last Name:</label>
+          <input
+            type="text"
+            placeholder="Insert your last name..."
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            className="w-full mb-2 border border-gray-300 rounded-lg px-4 py-2"
+          />
+          {lastNameError && <p>{lastNameError}</p>}
 
-      <button onClick={onClose}>Close</button>
+          <button
+            type="submit"
+            className="bg-green-500 w-full text-white mt-2 py-2 px-4 rounded-lg"
+          >
+            Register Now
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
